a8750c0a86e5b6cfdac42d2acf8c746505892434
Matilde Carvalho <75025133+uc2019233490@users.noreply.github.com>
2023-08-09 20:59:59 +0100

    Update bisection2.m

--- bisection2.m
+++ bisection2.m
@@ -1,25 +1,68 @@
-function c = bisection2(f, a, b, tol)
-    
-    if f(a)*f(b) > 0
-        error('O intervalo não contém nenhuma raiz');
-    end
+function [c, iter] = bisection2(f, a, b, tol)
 
-    %tol = 1e-4; % Define a tolerância (critério de paragem)
-    
-    for k = 1:kmax
-        c = (a+b)/2; % Primeiro ponto médio
-        if f(c) == 0 % Para se houver uma raíz
-            return
-        end
-    end
+% A função bisection2 recebe a função f, definida como função anónima,
+% e os valores a e b, que definem os limites do intervalo onde se encontra
+% uma solução de f e a tolerância.
 
-    if (b-a)/2 < tol % Valor abaixo da tolerância
-        return
+kmax = 100;
+count = 0;
+
+% Verificar se os valores são válidos
+if f(a)*f(b) > 0
+    fprintf('\nERRO! O intervalo não contém nenhuma ou mais do que uma raiz\n');
+    c = NaN;
+    iter = 0;
+    return;
+end
+
+% Representar função que se está a trabalhar
+% floor e ceil utilizados por motivos visuais
+
+str = regexprep(func2str(f),'^@\([^\)]\)\s*',''); % remover o @(x)
+
+x_plot = linspace(floor(a)-1,ceil(b)+1,1000);
+figure("Name","Método das Bissecções")
+plot(x_plot,f(x_plot),'k'); 
+title(sprintf('f(x) = %s', str));
+xlabel('x');ylabel('f(x)');hold on; grid on;
+plot(x_plot,zeros(1,length(x_plot)),'k:');
+
+% Representar máximo e mínimo definido
+p(1) = plot(a,0,'b*','MarkerSize',6);
+p(2) = plot(b,0,'r*','MarkerSize',6);
+
+legend(p([1 2]),"Mínimo","Máximo");
+
+% Primeiro ponto médio
+c = (a+b)/2;
+p(3) = plot(c,0,'go','MarkerFaceColor','g','MarkerSize',6);
+
+for k = 1:kmax
+    delete(p(3));
+    p(3) = plot(c,0,'yo','MarkerFaceColor','k','MarkerSize',2);
+    
+    % Novo ponto médio
+    c = (a+b)/2;
+    
+    p(3) = plot(c,0,'go','MarkerFaceColor','g','MarkerSize',6);
+    legend(p([1 2]),"Mínimo","Máximo");
+    
+    % 2 frames por segundo
+    pause(0.5);
+    
+    count = count +1;
+    
+    % Caso se tenha encontrado uma raíz ou tolerância correta
+    if (f(c) == 0 || (b-a)/2 < tol)
+        iter = count;
+        legend(p([1 2 3]),"Mínimo","Máximo","Solução");
+        return;
     end
- 
-    if f(b)*f(c) > 0 % Verifica mudança de sinal
-        b = c; % Ajusta o limite do intervalo
+    
+    % Verificar mudança de sinal e ajustar intervalo de trabalho
+    if f(b)*f(c) > 0
+        b = c;
     else
-        a=c;
+        a = c;
     end
 end
